6ff43fe26940fe39e07f0df2a0f5271bc66bb661
John Wason <wason@wasontech.com>
2022-12-31 03:32:02 -0500

    Use pyproject.toml

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 from setuptools import setup, find_packages, find_namespace_packages
 
 setup(
-    name='pyri-robotics',
-    version='0.1.0',
-    description='PyRI Teach Pendant Robotics Plugin',
-    author='John Wason',
-    author_email='wason@wasontech.com',
-    url='http://pyri.tech',
     package_dir={'': 'src'},
     packages=find_namespace_packages(where='src'),
     include_package_data=True,
@@ -14,23 +8,5 @@ setup(
         'pyri.robotics.robotics_jog_service': ['*.robdef','*.yml'],
         'pyri.robotics.robotics_motion_service': ['*.robdef','*.yml']
     },
-    zip_safe=False,
-    install_requires=[
-        'pyri-common',
-        'qpsolvers',
-        'scipy',
-        'toppra'
-    ],
-    tests_require=['pytest','pytest-asyncio'],
-    extras_require={
-        'test': ['pytest','pytest-asyncio']
-    },
-    entry_points = {
-        'pyri.plugins.sandbox_functions': ['pyri-robotics-sandbox-functions=pyri.robotics.sandbox_functions:get_sandbox_functions_factory'],
-        'pyri.plugins.device_type_adapter': ['pyri-robotics-type-adapter = pyri.robotics.device_type_adapter:get_device_type_adapter_factory'],
-        'pyri.plugins.blockly': ['pyri-robotics-plugin-blockly=pyri.robotics.blockly:get_blockly_factory'],
-        'console_scripts': ['pyri-robotics-jog-service = pyri.robotics.robotics_jog_service.__main__:main',
-            'pyri-robotics-motion-service = pyri.robotics.robotics_motion_service.__main__:main'],
-        'pyri.plugins.service_node_launch': ['pyri-robotics-launch = pyri.robotics.service_node_launch:get_service_node_launch_factory']
-    }
+    zip_safe=False
 )
